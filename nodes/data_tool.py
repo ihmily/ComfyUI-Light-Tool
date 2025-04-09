b4cacf760c425442a0d7d5998b5e43a487ef28a0
ihmily <114978440+ihmily@users.noreply.github.com>
2025-04-09 11:49:05 +0800

    feat: add json serialize and deserialize node

--- nodes/data_tool.py
+++ nodes/data_tool.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 
 class AnyType(str):
@@ -17,7 +18,7 @@ class KeyValue:
     def INPUT_TYPES(cls):
         return {
             "required": {
-                "data": ("STRING", {"default": '{"key": "This is value"}', "multiline": False}),
+                "data": ("STRING", {"default": '{"key": "This is value"}', "multiline": True}),
                 "key": ("STRING", {"default": "key", "multiline": False})
             }
         }
@@ -26,20 +27,108 @@ class KeyValue:
     RETURN_NAMES = ("value",)
     FUNCTION = "key_value"
     CATEGORY = 'ComfyUI-Light-Tool/DataProcessing'
-    DESCRIPTION = "Get values from JSON"
+    DESCRIPTION = "Get values from JSON string"
 
     @staticmethod
     def key_value(data, key):
-        json_data = json.loads(data)
-        value = json_data.get(key, '')
-        return (value, )
+        try:
+            json_data = json.loads(data)
+        except json.JSONDecodeError:
+            return ''
+
+        value = json_data
+        parts = key.split('.')
+        for part in parts:
+            key_part = re.match(r'^([^\[]*)', part).group(1)
+            indices = re.findall(r'\[(\d+)\]', part)
+
+            if key_part:
+                if isinstance(value, dict):
+                    value = value.get(key_part, '')
+                else:
+                    return ''
+                if value == '':
+                    return ''
+
+            for index_str in indices:
+                if isinstance(value, list):
+                    try:
+                        index = int(index_str)
+                    except ValueError:
+                        return ''
+                    if 0 <= index < len(value):
+                        value = value[index]
+                    else:
+                        return ''
+                else:
+                    return ''
+        return (value if value != '' else '',)
+
+
+class SerializeJsonObject:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "json_object": (any_type, {"default": json.loads('{"key": "This is value"}'), "defaultInput": True}),
+            }
+        }
+
+    RETURN_TYPES = (any_type,)
+    RETURN_NAMES = ("json_str",)
+    FUNCTION = "get_json_str"
+    CATEGORY = 'ComfyUI-Light-Tool/DataProcessing'
+    DESCRIPTION = "Convert a JSON object to a JSON string"
+
+    @staticmethod
+    def get_json_str(json_object):
+        try:
+            json_str = json.dumps(json_object, ensure_ascii=False)
+        except json.JSONDecodeError:
+            return '{}'
+        return (json_str,)
+
+
+class DeserializeJsonString:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "json_str": ("STRING", {"default": '{"key": "This is value"}', "multiline": True}),
+            }
+        }
+
+    RETURN_TYPES = (any_type,)
+    RETURN_NAMES = ("json_object",)
+    FUNCTION = "get_json"
+    CATEGORY = 'ComfyUI-Light-Tool/DataProcessing'
+    DESCRIPTION = "Convert a JSON string to a JSON object"
+
+    @staticmethod
+    def get_json(json_str):
+        try:
+            json_dict = json.loads(json_str)
+            return (json_dict,)
+        except json.JSONDecodeError:
+            json_dict = {}
+        return (json_dict,)
 
 
 NODE_CLASS_MAPPINGS = {
 
-    "Light-Tool: KeyValue": KeyValue
+    "Light-Tool: KeyValue": KeyValue,
+    "Light-Tool: SerializeJsonObject": SerializeJsonObject,
+    "Light-Tool: DeserializeJsonString": DeserializeJsonString,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "Light-Tool: KeyValue": "Light-Tool: Get values from JSON",
+    "Light-Tool: SerializeJsonObject": "Light-Tool: Serialize a JSON object",
+    "Light-Tool: DeserializeJsonString": "Light-Tool: Deserialize a JSON string",
 }
